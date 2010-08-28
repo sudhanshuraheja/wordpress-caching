5a34fe01f44883de3a4a7a5a90d5b504ab1afc07
Sudhanshu Raheja <sudhanshu.raheja@vxtindia.com>
2010-08-28 02:13:07 +0000

    Added basic info

---
+++ 88things.php
@@ -0,0 +1,17 @@
+<?php
+
+	/*
+		Plugin Name: 88things
+		Plugin URI: http://vxtindia.com
+		Description: This plugin will take care of everything
+		Author: Sudhanshu Raheja
+		Version : 0.0.1
+		Author URI: http://vxtindia.com
+	*/
+
+	/*
+		This plugin needs to take care of the following functionality
+			1. Redirect all javascript, css and images to maxcdn
+	*/
+
+?>
