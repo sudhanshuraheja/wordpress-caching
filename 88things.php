903ed8285da7b1262169298563e70f63ed5c560a
Sudhanshu Raheja <sudhanshu.raheja@vxtindia.com>
2010-08-28 04:15:22 +0000

    Changed urls for JS and CSS except tinymce

--- 88things.php
+++ 88things.php
@@ -14,4 +14,50 @@
 			1. Redirect all javascript, css and images to maxcdn
 	*/
 
+	class WordPress88 {
+
+		private $site_url;
+		private $cdn_url;
+
+		public function __construct() {
+			$this->site_url = str_replace('http://', '', get_bloginfo('wpurl'));
+			$this->cdn_url = 'pvxtindianet.vercingetorixtec.netdna-cdn.com';
+		}
+
+		public function init() {
+			$this->useCDN();
+		}
+
+		public function display($data) {
+			echo '<pre style="font-size: 12px; background-color: #F7F7F7; border: 1px solid #CECECE; margin: 3px 3px 0px; padding: 6px;">';
+			var_dump($data);
+			echo '</pre>';
+		}
+
+		private function useCDN() {
+			$this->rewriteJavascript();
+			$this->rewriteCSS();
+		}
+
+		private function rewriteJavascript() {
+			add_action('script_loader_src', array($this, 'filterJavascriptForCDN'));
+		}
+
+		public function filterJavascriptForCDN($url) {
+			return str_replace($this->site_url, $this->cdn_url, $url);
+		}
+
+		private function rewriteCSS() {
+			add_action('style_loader_src', array($this, 'filterCSSForCDN'));
+		}
+
+		public function filterCSSForCDN($url) {
+			return str_replace($this->site_url, $this->cdn_url, $url);
+		}
+
+	}
+
+	$wordpress88 = new WordPress88();
+	$wordpress88->init();
+
 ?>
